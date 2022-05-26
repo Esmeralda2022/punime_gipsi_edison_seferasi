78d1fb29cc2a661712aed11136f23aee9f7421bd
Esmeralda2022 <104155509+Esmeralda2022@users.noreply.github.com>
2022-05-27 00:37:47 +0200

    Update Social.js

--- src/components/social/Social.js
+++ src/components/social/Social.js
@@ -5,8 +5,8 @@ import {FaFacebook} from 'react-icons/fa';
 function Social() {
   return (
     <div className='header_socials'>
-        <a href='https://www.instagram.com/?hl=en' target= '_blank'className='btn btn-outline-light insta'><AiOutlineInstagram/></a>
-        <a href='https://www.facebook.com/lite/' target= '_blank' className='btn btn-outline-light fb'><FaFacebook/></a>
+        <a href='https://www.instagram.com/?hl=en' className='btn btn-outline-light insta'><AiOutlineInstagram/></a>
+        <a href='https://www.facebook.com/lite/' className='btn btn-outline-light fb'><FaFacebook/></a>
     </div>
   )
 }
