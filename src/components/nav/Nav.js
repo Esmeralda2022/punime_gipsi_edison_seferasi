1961903792750514b480e1187ce0c1e5bbc988f3
Esmeralda2022 <104155509+Esmeralda2022@users.noreply.github.com>
2022-06-26 16:23:48 +0200

    Update Nav.js

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -7,9 +7,9 @@ function Nav() {
   const [activeNav, setActiveNav] = useState('');
   return (
     <nav>
-      <a href='#' onClick={()=> setActiveNav('#')} className= { activeNav === '#' ? 'active' : ''} ><AiOutlineHome/></a>
-      <a href='#about' onClick={()=> setActiveNav('#about')} className= { activeNav === '#about' ? 'active' : ''}><BiUser/></a>
-      <a href='#contact' onClick={()=> setActiveNav('#contact')} className= { activeNav === '#contact' ? 'active' : ''}><TiMessages/></a>
+      <a href='#'><AiOutlineHome/></a>
+      <a href='#about' ><BiUser/></a>
+      <a href='#contact'><TiMessages/></a>
     </nav>
   )
 }
