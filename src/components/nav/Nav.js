a5a5c55feb6e2e31f7207c236d29895b4d9318b1
Esmeralda2022 <104155509+Esmeralda2022@users.noreply.github.com>
2022-06-26 16:31:38 +0200

    Update Nav.js

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import{AiOutlineHome} from 'react-icons/ai';
 import{BiUser}  from 'react-icons/bi';
 import{TiMessages} from 'react-icons/ti';
